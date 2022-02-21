c16c9324b2202f095562bf626654c738ecffdf2c
Irel <Irel@Projekty>
2022-02-21 13:25:18 +0100

    2

--- Matrix_Cell_Tester/Core/Threads/CMD_thread.h
+++
@@ -1,9 +0,0 @@
-
-
-void CMD_task(void const * argument);
-void serial_send_string(char *str);
-void string_zero_fill(char *str);
-void cmd_execute(char * cmd);
-void Rs485_received_execute(char *cmd);
-
-
--- Matrix_Cell_Tester/Core/Threads/INOUT_thread.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include "cmsis_os.h"
-#include "stm32l1xx_hal.h"
-#include "main.h"
-
-void INOUT_task(void const * argument);
-uint8_t h_adress(void);
-void send_frame (void);
-void Set_Out(uint8_t data);
