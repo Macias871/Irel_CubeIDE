eb843ae630d2bf2377653f57dd8e8234f0142288
Irel <Irel@Projekty>
2022-10-10 15:06:44 +0200

    2

---
+++ SODIMM-IS25LP128-H723_v6/Debug/TouchGFX/generated/images/src/image_right_slide_menu_background_black.d
@@ -0,0 +1,4 @@
+TouchGFX/generated/images/src/image_right_slide_menu_background_black.o: \
+ ../TouchGFX/generated/images/src/image_right_slide_menu_background_black.cpp \
+ ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
---
+++ SODIMM-IS25LP128-H723_v6/TouchGFX/build/MINGW32_NT-6.2/generated/images/src/image_button_small_in_menu_on_green.d
@@ -0,0 +1,5 @@
+build/MINGW32_NT-6.2/generated/images/src/image_button_small_in_menu_on_green.o: \
+ generated/images/src/image_button_small_in_menu_on_green.cpp \
+ ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
